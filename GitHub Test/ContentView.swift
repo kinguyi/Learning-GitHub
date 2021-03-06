af36704891ab5fd10e653e3b3ff7d35af136a333
Uyi <ehiosujoni@gmail.com>
2021-03-06 20:32:25 +0100

    Initial Commit

---
+++ GitHub Test/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GitHub Test
+//
+//  Created by Jonathan Uyi Ehiosu on 06.03.21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GitHub Test/GitHub_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHub_TestApp.swift
+//  GitHub Test
+//
+//  Created by Jonathan Uyi Ehiosu on 06.03.21.
+//
+
+import SwiftUI
+
+@main
+struct GitHub_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
